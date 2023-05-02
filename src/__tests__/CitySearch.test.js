2935c7c6e58d00726aabc867b1bb19379d0525b9
Regina Phalangy <nschallenberg@gmx.net>
2023-05-02 12:10:27 +0200

    Create test 'renders a list of suggestions'

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -3,8 +3,15 @@ import { shallow } from 'enzyme';
 import CitySearch from '../CitySearch';
 
 describe('<CitySearch /> component', () => {
+
   test('render text input', () => {
     const CitySearchWrapper = shallow(<CitySearch />);
     expect(CitySearchWrapper.find('.city')).toHaveLength(1);
   });
+
+  test('renders a list of suggestions', () => {
+    const CitySearchWrapper = shallow(<CitySearch />);
+    expect(CitySearchWrapper.find('.suggestions')).toHaveLength(1);
+  });
+
 });
