ea3afd5a99c79b19094111cf49a3566472fbc34f
Regina Phalangy <nschallenberg@gmx.net>
2023-05-02 22:25:47 +0200

    Create test 'render number of events'

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  });
+
+});
