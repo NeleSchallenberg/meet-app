816d5598a085655768dd502b6eb3c890e89f106b
NeleSchallenberg <nschallenberg@gmx.net>
2023-05-10 15:20:34 +0200

    Create and implement errorAlert

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
 	state = {
 		query: 32,
+		errorText: '',
 	};
 
 	handleChange = (event) => {
 		let inputValue = event.target.value;
 		this.props.countEvent(inputValue);
 		this.setState({ query: inputValue });
+		if (inputValue < 1) {
+			this.setState({
+				errorText: 'Select number from 1 to 32',
+			});
+		} else {
+			return this.setState({
+				errorText: '',
+			});
+		}
 	};
 
 	render() {
@@ -23,6 +34,7 @@ class NumberOfEvents extends Component {
 					value={this.state.query}
 					onChange={this.handleChange}
 				/>
+				<ErrorAlert text={this.state.errorText} />
 			</div>
 		);
 	}
