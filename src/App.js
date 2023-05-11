b52d5f3d3b71ec6e257161d36666ec11415ca49f
NeleSchallenberg <nschallenberg@gmx.net>
2023-05-11 16:56:49 +0200

    Add verification and welcome screen

--- src/App.js
+++ src/App.js
@@ -3,27 +3,42 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
 import './nprogress.css';
 import { OfflineAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 
 class App extends Component {
 	state = {
 		events: [],
 		locations: [],
+		showWelcomeScreen: undefined,
 		numberOfEvents: 32,
 		selectedCity: 'all',
 		offlineText: '',
 	};
 
-	componentDidMount() {
+	async componentDidMount() {
 		this.mounted = true;
-		getEvents().then((events) => {
-			if (this.mounted) {
-				this.setState({ events, locations: extractLocations(events) });
-			}
-		});
+		const accessToken = localStorage.getItem('access_token');
+		const isTokenValid = (await checkToken(accessToken)).error
+			? false
+			: true;
+		const searchParams = new URLSearchParams(window.location.search);
+		const code = searchParams.get('code');
+		this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+		if ((code || isTokenValid) && this.mounted) {
+			getEvents().then((events) => {
+				if (this.mounted) {
+					this.setState({
+						events,
+						locations: extractLocations(events),
+					});
+				}
+			});
+		}
 	}
+
 	componentWillUnmount() {
 		this.mounted = false;
 	}
@@ -63,6 +78,8 @@ class App extends Component {
 	};
 
 	render() {
+		if (this.state.showWelcomeScreen === undefined)
+			return <div className='App' />;
 		return (
 			<div className='app'>
 				<OfflineAlert
@@ -79,6 +96,12 @@ class App extends Component {
 					countEvent={this.countEvent}
 				/>
 				<EventList events={this.state.events} />
+				<WelcomeScreen
+					showWelcomeScreen={this.state.showWelcomeScreen}
+					getAccessToken={() => {
+						getAccessToken();
+					}}
+				/>
 			</div>
 		);
 	}
